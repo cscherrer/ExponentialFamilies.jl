2ffa371362f780f4d2b2682b8bb7ec8e600229a9
Chad Scherrer <chad.scherrer@gmail.com>
2019-08-10 19:38:24 -0700

    working on iid

--- src/types.jl
+++ src/types.jl
@@ -19,4 +19,14 @@ function (fam::NaturalExponentialFamily{P,X})(η::P) where {P,X}
     NatExpFamDist{P,X}(fam, η)
 end
 
+export iid
+function iid(d::NatExpFamDist{P,X} ) where {P,X}
+    logh(xs :: AbstractArray{<:X}) = sum(d.fam.logh.(xs))
+    t(xs :: AbstractArray{<:X}) = reduce((a,b) -> a .+ b,d.fam.t.(xs))
+    a(η::P) = d.fam.a(η)
+
+    fam = NaturalExponentialFamily{P,AbstractArray{X}}(logh, t, a)
+    NatExpFamDist(fam,d.η)
 end
+    
+    
